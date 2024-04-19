2c4b2ca2e45aeeb457444d694b9f39dde2a2dcfd
DERISBOURG <derisbourgarnaud@gmail.com>
2024-04-19 13:44:38 +0200

    premier commit avant impl security

--- src/main/java/com/arnaud/api/exception/UserNotFondException.java
+++ src/main/java/com/arnaud/api/exception/UserNotFondException.java
@@ -1,7 +1,2 @@
-package com.arnaud.api.exception;
-
-public class UserNotFondException extends RuntimeException {
-    public UserNotFondException(String message) {
-        super(message);
-    }
+package com.arnaud.api.exception;public class UserNotFondException {
 }
